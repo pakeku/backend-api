b535c5c9b10862530374aeced0f4d7743c388afd
pakeku <elina13@wgu.edu>
2025-05-17 00:40:31 -0400

    use err message returned

--- src/utils/git-user-name.ts
+++ src/utils/git-user-name.ts
@@ -1,12 +1,13 @@
 import { execSync } from 'child_process';
 
 function getGitUserName(): string {
-    try {
-        const name = execSync('git config --get user.name', { encoding: 'utf8' }).trim();
-        return name || 'unknown';
-    } catch (err) {
-        return 'unknown';
-    }
+  try {
+    const name = execSync('git config --get user.name', { encoding: 'utf8' }).trim();
+    return name || 'unknown';
+  } catch (err) {
+    console.info('Git user name not found, returning "unknown"', err);
+    return 'unknown';
+  }
 }
 
 export default getGitUserName;
