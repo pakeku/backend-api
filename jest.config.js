012a894d009b3ca7926ce0b9b4a070fdc4cc127c
pakeku <elina13@wgu.edu>
2025-05-17 11:56:50 -0400

    change config to use module.exports instead of export default

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-export default {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
   testEnvironment: 'node',
   transform: {
-    '^.+\.tsx?$': ['ts-jest', {}],
+    '^.+\\.tsx?$': ['ts-jest', {}],
   },
 };
